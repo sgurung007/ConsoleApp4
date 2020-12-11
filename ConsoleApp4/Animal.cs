3fa89d7cd894d46f2996d2b94e8208989afadb6d
suraj gurung <sgrg007@gmail.com>
2020-12-11 15:01:02 -0600

    Add project files.

---
+++ ConsoleApp4/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Animal
+    {
+        public int MyInt { get; set; }
+        public double MyDouble { get; set; } 
+        public char MyChar { get; set; }
+        public bool MyBool { get; set; }
+
+        public Animal(int myInt=0, double myDouble=0.00, char myChar='A', bool myBool=true)
+        {
+            MyInt = myInt;
+            MyDouble = myDouble;
+            MyChar = myChar;
+            MyBool = myBool;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            Console.WriteLine("my animal int {0}, " +
+                "double {1}, char {2}, bool {3}",animal1.MyInt,
+                animal1.MyDouble,animal1.MyChar, animal1.MyBool);
+
+
+            int myIntFromDobule = (int)animal1.MyDouble;
+            Console.WriteLine("my integer from double is: ",myIntFromDobule);
+
+            //console.writeline("enter you name: ");
+            //string namestring = console.readline();
+            //console.writeline("welcome {0}.", namestring);
+
+            int myInt = 10;
+            myInt++;
+            Console.WriteLine(myInt);
+
+            Console.WriteLine(Math.Max(20,10));
+          
+        }
+    }
+}
+
+
+/*These are my notes 
+ * Built in methods for converstion
+ * Convert.ToBoolean
+ * Convert.ToDobule
+ * Convert.ToString
+ * Convert.ToInt32(int)
+ * Convert.ToInt64(long)
+ * 
+ * + addition
+ * - subtraction
+ * * multiplication
+ / division
+    % modulus
+    ++ increment
+    -- decrement
+
+ * 
+ */
